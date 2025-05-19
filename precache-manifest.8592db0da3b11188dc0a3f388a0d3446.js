5a492893e7a6e63fd9151a06e06e9c47f8cc499d
Daniel Reeves <dreeves@beeminder.com>
2025-05-18 23:53:51 -0700

    Updates

---
+++ precache-manifest.8592db0da3b11188dc0a3f388a0d3446.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a10e9897b3d361c4ff76840cabcbc58",
+    "url": "/reciplier/index.html"
+  },
+  {
+    "revision": "57413faa61c279325eed",
+    "url": "/reciplier/static/css/main.075bc62a.chunk.css"
+  },
+  {
+    "revision": "84f1fdcd7690d4cdb614",
+    "url": "/reciplier/static/js/2.c98e45b7.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/reciplier/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "57413faa61c279325eed",
+    "url": "/reciplier/static/js/main.768498e6.chunk.js"
+  },
+  {
+    "revision": "423c8c92a83fd1d30c76",
+    "url": "/reciplier/static/js/runtime~main.07e1c579.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reciplier/static/media/logo.06e73328.svg"
+  }
+]);
