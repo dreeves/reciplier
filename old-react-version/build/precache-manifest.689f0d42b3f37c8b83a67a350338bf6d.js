7ef90199f3cc757a61add6736bb97ff2cda0233d
Daniel Reeves <dreeves@beeminder.com>
2025-05-28 13:44:10 -0700

    Something with node_modeules I hate git so much

---
+++ old-react-version/build/precache-manifest.689f0d42b3f37c8b83a67a350338bf6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5277aa85d275a2698a6a1e0897908465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1c3b5889fec42565f8",
+    "url": "/static/css/main.55c4005f.chunk.css"
+  },
+  {
+    "revision": "e4c8d66a19440d9f9e9f",
+    "url": "/static/js/2.b291972e.chunk.js"
+  },
+  {
+    "revision": "4a1c3b5889fec42565f8",
+    "url": "/static/js/main.3c0ada78.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
